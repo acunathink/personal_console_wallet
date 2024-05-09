2d6a17b62f17980f026f2a8eb409281ab5b0e3a9
Тимофей Карпов <acuna-think@yandex.ru>
2024-05-09 08:35:29 +0300

    add PersonalFinanceWallet prototype

--- wallet.py
+++ wallet.py
@@ -1,3 +1,6 @@
+import json
+import sys
+
 
 HELLO = "Wallet - application for accounting personal income and expenses"
 HINT_USAGE = (
@@ -24,7 +27,57 @@ HINT_USAGE = (
 DEFAULT_FILE = "data.json"
 
 
+class PersonalFinanceWallet:
+    def __init__(self) -> None:
+        self.data: dict = self.load_data()
+        self.file = DEFAULT_FILE
+
+    def load_data(self) -> dict:
+        try:
+            with open(self.file, "r") as file:
+                data: dict = json.load(file)
+                print("\t\t~ wallet ready ~")
+        except FileNotFoundError:
+            data: dict = {"balance": 0, "income": [], "expenses": []}
+            print("\t\t~ wallet empty ~")
+
+        return data
+
+    def save_data(self):
+        with open(self.file, "w") as file:
+            json.dump(self.data, file, indent=2)
+
+    def print_balance(self, command):
+        pass
+
+    def add_record(self, command):
+        pass
+
+    def edit_record(self, category, index, amount, description):
+        pass
+
+    def search_records(self, category=None, date=None, amount=None):
+        pass
+
 
 if __name__ == '__main__':
     print(HELLO)
-    print(HINT_USAGE)
+    wallet = PersonalFinanceWallet()
+    while True:
+        command = input("Wallet-> ").split()
+        if len(command) < 1:
+            continue
+        match command[0]:
+            case 'show':
+                pass
+            case 'add':
+                pass
+            case 'edit':
+                pass
+            case 'search':
+                pass
+            case 'exit':
+                sys.exit()
+            case _:
+                print(f'Unknown COMMAND: "{"".join(command)}"')
+                print(HINT_USAGE)
